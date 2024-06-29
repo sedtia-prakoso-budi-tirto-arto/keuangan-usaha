333927025decd4ab5607d93459e501e0f82dcb41
sedtia-prakoso-budi-tirto-arto <prakosobudi2k@gmail.com>
2024-06-29 18:14:57 +0700

    updated

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 /* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  assetsInclude: ['service-worker.js'],
   // pwa: {
   //   name: 'Modal Harian',
   //   themeColor: '#4DBA87',
